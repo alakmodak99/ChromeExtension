cfb557214d387ed10115f290a76ad741c11d140a
alakmodak99 <alakmodak99@gmail.com>
2023-02-22 03:06:29 +0530

    Chrome Extention Update

--- Script.js
+++ Script.js
@@ -1,48 +1,59 @@
 console.log("connected");
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log("connected", request?.message);
+  console.log("connected", request?.message, request?.start);
   let SetTimeOut;
   if (request?.message) {
     SetTimeOut = setTimeout(() => {
-      Connect();
-    }, [5000]);
+      Connect(request?.message);
+    }, request?.start);
   } else {
     clearTimeout(SetTimeOut);
   }
 });
 
-const Connect = () => {
+const Connect = (event) => {
   const data =
     document &&
     document?.getElementsByClassName(
       "artdeco-button artdeco-button--2 artdeco-button--secondary ember-view"
     );
   let x,
+    count = 0,
     i = 0,
     y = data?.length;
-  x = setInterval(() => {
+  x = setInterval(async () => {
     let key = data[i];
     if (key?.innerText == "Connect") {
+      count++;
       key?.click();
-      setTimeout(() => {
-        document
-          .getElementsByClassName(
-            "artdeco-pill artdeco-pill--slate artdeco-pill--3 artdeco-pill--choice ember-view mt2"
-          )?.[0]
-          ?.click();
-        document
-          .getElementsByClassName(
-            "artdeco-button artdeco-button--2 artdeco-button--primary ember-view"
-          )?.[0]
-          ?.click();
-        document
-          .getElementsByClassName(
-            "artdeco-button artdeco-button--2 artdeco-button--primary ember-view ml1"
-          )?.[0]
-          ?.click();
-      }, 900);
+      await SetTimeO();
     }
+    if (!event) clearInterval(x);
     i++;
     if (i == y) clearInterval(x);
-  }, 1600);
+    console.log(count, "Count");
+    chrome.runtime.sendMessage({ count, end: i==y }, (res) => {});
+  }, 1700);
 };
+
+function SetTimeO() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      document
+        .getElementsByClassName(
+          "artdeco-pill artdeco-pill--slate artdeco-pill--3 artdeco-pill--choice ember-view mt2"
+        )?.[0]
+        ?.click();
+      document
+        .getElementsByClassName(
+          "artdeco-button artdeco-button--2 artdeco-button--primary ember-view"
+        )?.[0]
+        ?.click();
+      document
+        .getElementsByClassName(
+          "artdeco-button artdeco-button--2 artdeco-button--primary ember-view ml1"
+        )?.[0]
+        ?.click();
+    }, 900);
+  });
+}
--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 document.addEventListener("DOMContentLoaded", RunScript);
+function RanDomNumer(min, max) {
+  return Math.floor(min + Math.random() * (max - min + 1));
+}
 function RunScript() {
+  let random = RanDomNumer(6, 9);
   let start = false;
-  let btn = document?.getElementById("btn").addEventListener("click", () => {
-    let count = 5,
-      x;
+  document?.getElementById("btn").addEventListener("click", () => {
     start = !start;
     if (start) {
       document.getElementById("coutnText").style.display = "block";
       document.getElementById("ConnectText").style.display = "none";
       document.getElementById("btn").innerText = "Stop Connecting";
       document.getElementById("btn").style.border = "1px solid red";
-      x = setInterval(() => {
-        document.getElementById("count").innerText = count--;
-        if (count < 0) {
-          clearInterval(x);
-          document.getElementById("coutnText").innerText = "Started";
-        }
-      }, 1000);
     } else {
       document.getElementById("coutnText").style.display = "none";
       document.getElementById("ConnectText").style.display = "block";
       document.getElementById("btn").innerText = "Start Connecting";
       document.getElementById("btn").style.border = "1px solid green";
     }
-    popup(start);
+    popup(start, random);
+  });
+  chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+    document.getElementById("count").innerText = Math.ceil(msg?.count/2);
+    if(msg?.end){
+        chrome.management.onDisabled.addListener((a)=>{
+            console.log(a?.name, "disabled");
+        })
+    }
+
   });
 }
-function popup(data) {
+function popup(data, num) {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     var activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: data });
+    chrome.tabs.sendMessage(activeTab.id, { message: data, start: num });
   });
 }
